1ea29ccf7e0e41de674c2a6a32b0bc59ae1bddf5
Irina <irinavasilitav@gmail.com>
2024-09-12 17:42:35 +0300

    Add initial view logic for digit recognition

--- digits_recognition/recognition/views.py
+++ digits_recognition/recognition/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def recognize_digit(request):
+    if request.method == 'POST':
+        if 'canvas_data' in request.POST:
+            # Get the base64 data from the POST request
+            canvas_data = request.POST['canvas_data']
+    # Render the template
+    return render(request, 'recognition/draw.html')
